e5d6e75f41835052e01ed744a3727ad75cae8883
Marc313 <77342337+Marc313@users.noreply.github.com>
2023-04-19 19:20:08 +0200

    Rider initialization + Tutorial Exercises

---
+++ UVWD_CombatGame/Source/UVWD_CombatGame/Character/Public/Enemy.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Enemy.generated.h"
+
+UCLASS()
+class UVWD_COMBATGAME_API AEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	void Debug();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
